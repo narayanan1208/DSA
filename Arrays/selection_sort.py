54f2b06cde92798833c379539933e7a77caccbed
narayanan1208 <narayanan6647@gmail.com>
2025-02-15 12:53:11 +0530

    Updated selection sort example

--- Arrays/selection_sort.py
+++ Arrays/selection_sort.py
@@ -15,7 +15,6 @@ for i in range(n-1):
     for j in range(i+1, n):
         if my_array[j] < my_array[min_index]:
             min_index = j
-    min_value = my_array.pop(min_index)
-    my_array.insert(i, min_value)
+    my_array[i], my_array[min_index] = my_array[min_index], my_array[i] 
 
 print("Sorted array : ", my_array)
